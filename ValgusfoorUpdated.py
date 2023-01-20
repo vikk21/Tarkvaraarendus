9d573ba4f308047e87cceea1dedf9cf1b19d992e
vikk21 <toomas.lellep@vikk.ee>
2023-01-20 12:50:13 +0200

    test

--- ValgusfoorUpdated.py
+++ ValgusfoorUpdated.py
@@ -9,7 +9,11 @@ pygame.draw.rect(screen, [255, 255, 255], [90, 10, 130, 280], 2) #loob ristküli
 pygame.draw.circle(screen, [255, 0, 0], [155,63], 40, 0) #loob ringi [värv] ,[(screen, värv, tsentri_pos, raadius, joone_paksus)
 pygame.draw.circle(screen, [236, 240, 0], [155,152], 40, 0) #loob ringi [värv] ,[(screen, värv, tsentri_pos, raadius, joone_paksus)
 pygame.draw.circle(screen,[0, 255, 0], [155,240], 40, 0) #loob ringi [värv] ,[(screen, värv, tsentri_pos, raadius, joone_paksus)
-pygame.draw.line,[1, 15, 400]
+pygame.draw.line(screen, [255,255,255], [155,290], [155,500], 2)
+pygame.draw.polygon(screen, [0, 0, 0] , [[90,500],[220,500],[290,600],[10,600],[90,500]], 2) #Joon ümber
+pygame.draw.polygon(screen, [0, 123, 255], [[90,500],[220,500],[243,533],[64,533],[90,500]], 0)
+pygame.draw.polygon(screen, [0, 0, 0], [[64,533],[243,533],[266,566],[38,566],[64,533]], 0)
+pygame.draw.polygon(screen, [255, 255, 255], [[38,566],[266,566],[290,600],[10,600],[38,566]], 0)
 pygame.display.flip() #värskendab aknaid
 running = True #Hoiab programmi tööle
 
